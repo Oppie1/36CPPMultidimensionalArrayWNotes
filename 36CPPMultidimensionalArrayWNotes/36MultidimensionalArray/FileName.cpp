72930e6a9b2fb0ac1d9d3272228c3f53b7d4904b
Oppie1 <atindall729@gmail.com>
2025-07-06 16:25:26 -0700

    Add files via upload

---
+++ 36CPPMultidimensionalArrayWNotes/36MultidimensionalArray/FileName.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int sally[2][3] = { {2, 3, 4},{8, 9, 10} };
+	cout << sally[1][1];
+
+	//The first array [2] means there are 2 rows in the array. 2 "{  }" and 
+	//The second array [3] means there are 3 columns in the array so the elements in the arrays" { , , }
+	//So  the output of 0,0 would be 2 while 1,1 would be 8. 1,1 is 9. 1,2 is 10. 0,1 is 3
+	return 0;
+}
+
