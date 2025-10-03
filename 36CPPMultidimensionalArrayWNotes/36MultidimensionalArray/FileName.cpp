2e199c6f5252979d23caac7d15dd73a33d4dbd8c
Oppie1 <atindall729@gmail.com>
2025-10-03 12:00:40 -0500

    Fix formatting issues in FileName.cpp

--- 36CPPMultidimensionalArrayWNotes/36MultidimensionalArray/FileName.cpp
+++ 36CPPMultidimensionalArrayWNotes/36MultidimensionalArray/FileName.cpp
@@ -7,7 +7,8 @@ int main() {
 
 	//The first array [2] means there are 2 rows in the array. 2 "{  }" and 
 	//The second array [3] means there are 3 columns in the array so the elements in the arrays" { , , }
-	//So  the output of 0,0 would be 2 while 1,1 would be 8. 1,1 is 9. 1,2 is 10. 0,1 is 3
+	//So  the output of 0,0 would be 2 while 1,1 would be 9. 1,1 is 9. 1,2 is 10. 0,1 is 3
 	return 0;
 }
+
 
